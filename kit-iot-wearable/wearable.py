dd18d5b6e4ab4af21547985a232cf962ba7f0de3
Vitor Leal <vitorleal1@gmail.com>
2015-01-17 00:14:18 -0200

    add lib methods

---
+++ kit-iot-wearable/wearable.py
@@ -0,0 +1,44 @@
+class Wearable(object):
+    """Wearable class"""
+
+    def __init__(self, name='wearable'):
+        self.name = name
+        self.devices = []
+
+        self.connect()
+
+    def find_wearable():
+        """Find nearby wearables"""
+        pass
+
+    def connect(self):
+        """Connect to a wearable device"""
+        print "Connectiong to Wearable"
+
+    def disconnect():
+        pass
+
+    def send_commad():
+        pass
+
+    def on_data():
+        pass
+
+    def lef_off():
+        pass
+
+    def lef_on(self, color='GREEN', value=255):
+        pass
+
+    def play_music(self, music='MARIO'):
+        pass
+
+    def get_luminosity():
+        pass
+
+    def get_temperature():
+        pass
+
+    def is_connected():
+        pass
+
